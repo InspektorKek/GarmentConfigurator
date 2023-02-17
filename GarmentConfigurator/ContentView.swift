52219a19fd4145609f100e86c6cd417b9685f60e
Mikhail Borisov <borisovdiag@gmail.com>
2023-02-17 11:56:14 +0100

    Update gitignore and InstallSwiftLint

--- GarmentConfigurator/ContentView.swift
+++ GarmentConfigurator/ContentView.swift
@@ -8,34 +8,38 @@
 import SwiftUI
 import RealityKit
 
-struct ContentView : View {
+struct ContentView: View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
+
     func makeUIView(context: Context) -> ARView {
-        
+
         let arView = ARView(frame: .zero)
-        
+
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        do {
+            let boxAnchor = try Experience.loadBox()
+
+            // Add the box anchor to the scene
+            arView.scene.anchors.append(boxAnchor)
+        } catch {
+            print(error)
+        }
+
         return arView
-        
+
     }
-    
+
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
+
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
