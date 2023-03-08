7c1b4e9254fb388849f697cbda462997500a2c22
Mikhail Borisov <borisovdiag@gmail.com>
2023-03-08 23:30:09 +0100

    Update App constant links

--- GarmentConfigurator/Core/AppConstants.swift
+++ GarmentConfigurator/Core/AppConstants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum AppConstants {
     enum Links {
-        static let termsAndConditions = "https://google.com"
-        static let privacyPolicy = "https://apple.com"
+        static let termsAndConditions = "https://doc-hosting.flycricket.io/snaparment-terms-of-use/b0b0305c-1f98-4dc5-96d3-644ca493c19e/terms"
+        static let privacyPolicy = "https://doc-hosting.flycricket.io/snaparment-privacy-policy/93eadf22-9501-481f-aa30-e63a9493211f/privacy"
     }
 }
